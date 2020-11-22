9947ec5efe68f976e800918c81a652c0fc3c4725
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2020-11-22 01:09:37 +0100

    koncepcja

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::string bee =  "\uD83D";
+    std::string bee1 = "\U0001F331";
+//    std::cout << bee << std::endl;
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            std::cout << bee1;
+
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
+
