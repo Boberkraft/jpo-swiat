0595fff0b1af01ef19d876de98edcd2d714c5b3d
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2020-11-22 15:04:55 +0100

    potworzenie niezbednych plików

---
+++ src/organizm/roslina/Koka.h
@@ -0,0 +1,16 @@
+//
+// Created by johnny on 22/11/2020.
+//
+
+#ifndef JPO_SWIAT_KOKA_H
+#define JPO_SWIAT_KOKA_H
+
+
+#include "../Roslina.h"
+
+class Koka : public Roslina {
+
+};
+
+
+#endif //JPO_SWIAT_KOKA_H
---
+++ src/organizm/roslina/Mlecz.h
@@ -0,0 +1,16 @@
+//
+// Created by johnny on 22/11/2020.
+//
+
+#ifndef JPO_SWIAT_MLECZ_H
+#define JPO_SWIAT_MLECZ_H
+
+
+#include "../Roslina.h"
+
+class Mlecz : public Roslina {
+
+};
+
+
+#endif //JPO_SWIAT_MLECZ_H
